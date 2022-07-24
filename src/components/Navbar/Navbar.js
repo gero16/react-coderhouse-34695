c93e5a075812a3c1a4ec80718c40f8fe19c108aa
gero16 <geronicola1696@gmail.com>
2022-07-23 21:06:04 -0300

    menu e-commerce - Geronimo Nicola

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,31 @@
+import "./Navbar.css"
+import Logo from '../../img/logo.jpg';
+import Button from "../Button/Button"
+
+const Navbar = () => {
+    
+    const text = "Click";
+
+    const handleClick = () => {
+        console.log(text)
+    }
+
+    return ( 
+        <nav className="Navbar"> 
+            <div className="div-logo">
+                <img src={Logo} alt="Imagen del logo de la tienda"/>
+            </div>
+
+            <div className="div-menu"> 
+                <Button label="Celulares"/> 
+                <Button label ="Notebooks"/>  
+                <Button label="Tablets" />  
+                <Button label="PC-Gamer" />  
+                <Button label="Auriculares" />  
+            </div>
+        </nav>
+    )
+}
+
+
+export default Navbar
